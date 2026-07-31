37688fe80807f4fd3a657f17b65b0b1373dfc9d8
yumiratoni-hue <yumiratoni@gmail.com>
2026-07-31 12:50:21 +0700

    Update route.js

--- app/api/order/route.js
+++ app/api/order/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { db } from "@/lib/firebase";
+import { db } from "../../../lib/firebase";
 import { doc, getDoc, updateDoc, addDoc, collection, serverTimestamp } from "firebase/firestore";
 
 export async function POST(req) {
