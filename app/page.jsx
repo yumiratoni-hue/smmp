07298d584261f7145edcfd00b060f5fcf5898b57
yumiratoni-hue <yumiratoni@gmail.com>
2026-07-31 07:34:39 +0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  // Otomatis mengarahkan pengunjung dari domain.com ke domain.com/dashboard
   redirect("/dashboard");
 }
